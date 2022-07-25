3890e78e33f27bbaaea379d4534848d316c3d693
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-25 04:03:31 -0700

    Restructure library

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -9,21 +9,21 @@ var BIN = path.join(__dirname, '..', '..', 'bin');
 describe('cli', function () {
   describe('happy path', function () {
     it('basic command - sequential', function (done) {
-      spawn(path.join(BIN, 'dis-then-dat.js'), ['--silent', 'npm --version', 'node --version'], { encoding: 'utf8' }, function (err, res) {
+      spawn(path.join(BIN, 'dis-then-dat.js'), ['--silent', 'echo "hello"', 'node --version'], { encoding: 'utf8' }, function (err, res) {
         assert.ok(!err);
         var lines = cr(res.stdout).split('\n');
-        assert.ok(isVersion(lines.slice(-3, -2)[0]));
+        assert.equal(lines.slice(-3, -2)[0], 'hello');
         assert.ok(isVersion(lines.slice(-2, -1)[0], 'v'));
         done();
       });
     });
 
     it('basic command - parallel', function (done) {
-      spawn(path.join(BIN, 'dis-and-dat.js'), ['--silent', 'npm --version', 'node --version'], { encoding: 'utf8' }, function (err, res) {
+      spawn(path.join(BIN, 'dis-and-dat.js'), ['--silent', 'echo "hello"', 'node --version'], { encoding: 'utf8' }, function (err, res) {
         assert.ok(!err);
         var lines = cr(res.stdout).split('\n');
         var versions = lines.slice(-3, -1);
-        assert.ok(isVersion(versions[0]) || isVersion(versions[0], 'v'));
+        assert.ok(versions[0] === 'hello' || isVersion(versions[0], 'v'));
         assert.ok(isVersion(versions[1]) || isVersion(versions[1], 'v'));
         done();
       });
