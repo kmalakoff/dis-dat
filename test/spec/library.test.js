14b2fbf2825c2801f2537713984ea06f2fe73196
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 15:25:05 -0800

    CI workflow, and test suites

--- test/spec/library.test.js
+++ test/spec/library.test.js
@@ -1,5 +1,4 @@
 // remove NODE_OPTIONS from ts-dev-stack
-// biome-ignore lint/performance/noDelete: <explanation>
 delete process.env.NODE_OPTIONS;
 
 const assert = require('assert');
@@ -9,25 +8,47 @@ const cr = require('cr');
 const disDat = require('dis-dat');
 
 describe('library', () => {
-  describe('happy path', () => {
+  (() => {
+    // patch and restore promise
+    let rootPromise;
+    before(() => {
+      rootPromise = global.Promise;
+      global.Promise = require('pinkie-promise');
+    });
+    after(() => {
+      global.Promise = rootPromise;
+    });
+  })();
+
+  describe('sequential', () => {
     it('basic command', (done) => {
       disDat(['echo "hello"', 'node --version'], { concurrency: 1, encoding: 'utf8' }, (err, results) => {
-        assert.ok(!err);
+        assert.ok(!err, err ? err.message : '');
         assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
         assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
         done();
       });
     });
-    it('basic command (promises)', (done) => {
-      disDat(['echo "hello"', 'node --version'], { concurrency: Infinity, encoding: 'utf8' })
-        .then((results) => {
-          assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
-          assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
-          done();
-        })
-        .catch((err) => {
-          assert.ok(!err);
-        });
+    it('basic command (promises)', async () => {
+      const results = await disDat(['echo "hello"', 'node --version'], { concurrency: 1, encoding: 'utf8' });
+      assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
+      assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
+    });
+  });
+
+  describe('parallel', () => {
+    it('basic command', (done) => {
+      disDat(['echo "hello"', 'node --version'], { concurrency: Infinity, encoding: 'utf8' }, (err, results) => {
+        assert.ok(!err, err ? err.message : '');
+        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
+        assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
+        done();
+      });
+    });
+    it('basic command (promises)', async () => {
+      const results = await disDat(['echo "hello"', 'node --version'], { concurrency: Infinity, encoding: 'utf8' });
+      assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
+      assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
     });
   });
 });
