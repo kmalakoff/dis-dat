48a616d0a687a305d91050e3ee931a265871f4a9
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-31 19:35:18 -0800

    Allow suppressing errors when using bracketed commands in worker

--- test/spec/library.test.js
+++ test/spec/library.test.js
@@ -4,6 +4,10 @@ const cr = require('cr');
 
 const disDat = require('dis-dat');
 
+function isError(obj) {
+  return Object.prototype.toString.call(obj) === '[object Error]';
+}
+
 describe('library', () => {
   (() => {
     // patch and restore promise
@@ -40,6 +44,17 @@ describe('library', () => {
         done();
       });
     });
+    it('handles errors - suppresses in dtd', (done) => {
+      disDat(['echo "hello"', '{this is an error}', 'node --version'], { concurrency: 1, encoding: 'utf8' }, (err, results) => {
+        assert.ok(!err, err ? err.message : '');
+        assert.ok(results.length === 3);
+        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
+        assert.ok(!results[1].error);
+        assert.ok(isError(results[1].result));
+        assert.ok(isVersion(cr(results[2].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
+        done();
+      });
+    });
   });
 
   describe('parallel', () => {
@@ -62,6 +77,17 @@ describe('library', () => {
         assert.ok(results.length === 3);
         assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
         assert.ok(results[1].error);
+        assert.ok(isVersion(cr(results[2].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
+        done();
+      });
+    });
+    it('handles errors - suppresses in dad', (done) => {
+      disDat(['echo "hello"', '{this is an error}', 'node --version'], { concurrency: Infinity, encoding: 'utf8' }, (err, results) => {
+        assert.ok(!err, err ? err.message : '');
+        assert.ok(results.length === 3);
+        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0], 'hello');
+        assert.ok(!results[1].error);
+        assert.ok(isError(results[1].result));
         assert.ok(isVersion(cr(results[2].result.stdout).split('\n').slice(-2, -1)[0], 'v'));
         done();
       });
