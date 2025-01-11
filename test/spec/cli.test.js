a645d8ced012ef765c88474b73a0aa67d09a13a9
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-11 13:27:47 -0800

    Add getLines utility and refactor worker callback name

--- test/spec/cli.test.js
+++ test/spec/cli.test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 const path = require('path');
 const spawn = require('cross-spawn-cb');
+const getLines = require('../lib/getLines.cjs');
 const isVersion = require('is-version');
-const cr = require('cr');
 
 const BIN = path.join(__dirname, '..', '..', 'bin');
 const CLI_DTD = path.join(BIN, 'dtd.cjs');
@@ -13,9 +13,9 @@ describe('cli', () => {
     it('basic command - sequential', (done) => {
       spawn(CLI_DTD, ['--silent', 'echo "hello"', 'node --version'], { encoding: 'utf8' }, (err, res) => {
         if (err) return done(err);
-        const lines = cr(res.stdout).split('\n');
-        assert.equal(lines.slice(-3, -2)[0].split(':')[1], 'hello');
-        assert.ok(isVersion(lines.slice(-2, -1)[0].split(':')[1], 'v'));
+        const lines = getLines(res.stdout);
+        assert.equal(lines.slice(-2)[0], 'hello');
+        assert.ok(isVersion(lines.slice(-1)[0], 'v'));
         done();
       });
     });
@@ -23,52 +23,52 @@ describe('cli', () => {
     it('basic command - parallel', (done) => {
       spawn(CLI_DAD, ['--silent', 'echo "hello"', 'node --version'], { encoding: 'utf8' }, (err, res) => {
         if (err) return done(err);
-        const lines = cr(res.stdout).split('\n');
-        const versions = lines.slice(-3, -1);
-        assert.ok(versions[0].split(':')[1] === 'hello' || isVersion(versions[0].split(':')[1], 'v'));
-        assert.ok(versions[1].split(':')[1] === 'hello' || isVersion(versions[1].split(':')[1], 'v'));
+        const lines = getLines(res.stdout);
+        const versions = lines.slice(-2);
+        assert.ok(versions[0] === 'hello' || isVersion(versions[0], 'v'));
+        assert.ok(versions[1] === 'hello' || isVersion(versions[1], 'v'));
         done();
       });
     });
     it('handles errors - stops in dtd', (done) => {
       spawn(CLI_DTD, ['--silent', 'echo "hello"', 'this is an error', 'node --version'], { encoding: 'utf8' }, (err, _res) => {
         assert.ok(err.status !== 0);
-        const lines = cr(err.stdout).split('\n');
-        const versions = lines.slice(-3, -1);
+        const lines = getLines(err.stdout);
+        const versions = lines.slice(-2);
         assert.equal(versions.length, 2);
-        assert.equal(versions[0].split(':')[1], 'hello');
+        assert.equal(versions[0], 'hello');
         done();
       });
     });
     it('handles errors - suppresses in dtd', (done) => {
       spawn(CLI_DTD, ['--silent', 'echo "hello"', '{this is an error}', 'node --version'], { encoding: 'utf8' }, (err, res) => {
         assert.ok(!err);
-        const lines = cr(res.stdout).split('\n');
-        const versions = lines.slice(-3, -1);
+        const lines = getLines(res.stdout);
+        const versions = lines.slice(-2);
         assert.equal(versions.length, 2);
-        assert.equal(versions[0].split(':')[1], 'hello');
+        assert.equal(versions[0], 'hello');
         done();
       });
     });
     it('handles errors - stops in dad', (done) => {
       spawn(CLI_DAD, ['--silent', 'echo "hello"', 'this is an error', 'node --version'], { encoding: 'utf8' }, (err, _res) => {
         assert.ok(err.status !== 0);
-        const lines = cr(err.stdout).split('\n');
-        const versions = lines.slice(-3, -1);
+        const lines = getLines(err.stdout);
+        const versions = lines.slice(-2);
         assert.equal(versions.length, 2);
-        assert.ok(versions[0].split(':')[1] === 'hello' || isVersion(versions[0].split(':')[1], 'v'));
-        assert.ok(versions[1].split(':')[1] === 'hello' || isVersion(versions[1].split(':')[1], 'v'));
+        assert.ok(versions[0] === 'hello' || isVersion(versions[0], 'v'));
+        assert.ok(versions[1] === 'hello' || isVersion(versions[1], 'v'));
         done();
       });
     });
     it('handles errors - suppresses in dad', (done) => {
       spawn(CLI_DAD, ['--silent', 'echo "hello"', '{this is an error}', 'node --version'], { encoding: 'utf8' }, (err, res) => {
         assert.ok(!err);
-        const lines = cr(res.stdout).split('\n');
-        const versions = lines.slice(-3, -1);
+        const lines = getLines(res.stdout);
+        const versions = lines.slice(-2);
         assert.equal(versions.length, 2);
-        assert.ok(versions[0].split(':')[1] === 'hello' || isVersion(versions[0].split(':')[1], 'v'));
-        assert.ok(versions[1].split(':')[1] === 'hello' || isVersion(versions[1].split(':')[1], 'v'));
+        assert.ok(versions[0] === 'hello' || isVersion(versions[0], 'v'));
+        assert.ok(versions[1] === 'hello' || isVersion(versions[1], 'v'));
         done();
       });
     });
--- test/spec/library.test.js
+++ test/spec/library.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const isVersion = require('is-version');
-const cr = require('cr');
+const getLines = require('../lib/getLines.cjs');
 
 const disDat = require('dis-dat');
 
@@ -25,22 +25,22 @@ describe('library', () => {
     it('basic command', (done) => {
       disDat(['echo "hello"', 'node --version'], { concurrency: 1, encoding: 'utf8' }, (err, results) => {
         if (err) return done(err);
-        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
-        assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'v'));
+        assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
+        assert.ok(isVersion(getLines(results[1].result.stdout).slice(-2)[0], 'v'));
         done();
       });
     });
     it('basic command (promises)', async () => {
       const results = await disDat(['echo "hello"', 'node --version'], { concurrency: 1, encoding: 'utf8' });
-      assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
-      assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'v'));
+      assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
+      assert.ok(isVersion(getLines(results[1].result.stdout).slice(-2)[0], 'v'));
     });
     it('handles errors - stops in dtd', (done) => {
       disDat(['echo "hello"', 'this is an error', 'node --version'], { concurrency: 1, encoding: 'utf8' }, (err) => {
         assert.ok(!!err);
         const results = err.results;
         assert.ok(results.length === 2);
-        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
+        assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
         assert.ok(results[1].error);
         done();
       });
@@ -49,10 +49,10 @@ describe('library', () => {
       disDat(['echo "hello"', '{this is an error}', 'node --version'], { concurrency: 1, encoding: 'utf8' }, (err, results) => {
         if (err) return done(err);
         assert.ok(results.length === 3);
-        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
+        assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
         assert.ok(!results[1].error);
         assert.ok(isError(results[1].result));
-        assert.ok(isVersion(cr(results[2].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'v'));
+        assert.ok(isVersion(getLines(results[2].result.stdout).slice(-2)[0], 'v'));
         done();
       });
     });
@@ -62,23 +62,23 @@ describe('library', () => {
     it('basic command', (done) => {
       disDat(['echo "hello"', 'node --version'], { concurrency: Infinity, encoding: 'utf8' }, (err, results) => {
         if (err) return done(err);
-        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
-        assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'v'));
+        assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
+        assert.ok(isVersion(getLines(results[1].result.stdout).slice(-2)[0], 'v'));
         done();
       });
     });
     it('basic command (promises)', async () => {
       const results = await disDat(['echo "hello"', 'node --version'], { concurrency: Infinity, encoding: 'utf8' });
-      assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
-      assert.ok(isVersion(cr(results[1].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'v'));
+      assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
+      assert.ok(isVersion(getLines(results[1].result.stdout).slice(-2)[0], 'v'));
     });
     it('handles errors - continues in dad', (done) => {
       disDat(['echo "hello"', 'this is an error', 'node --version'], { concurrency: Infinity, encoding: 'utf8' }, (err, results) => {
         if (err) return done(err);
         assert.ok(results.length === 3);
-        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
+        assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
         assert.ok(results[1].error);
-        assert.ok(isVersion(cr(results[2].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'v'));
+        assert.ok(isVersion(getLines(results[2].result.stdout).slice(-2)[0], 'v'));
         done();
       });
     });
@@ -86,10 +86,10 @@ describe('library', () => {
       disDat(['echo "hello"', '{this is an error}', 'node --version'], { concurrency: Infinity, encoding: 'utf8' }, (err, results) => {
         if (err) return done(err);
         assert.ok(results.length === 3);
-        assert.equal(cr(results[0].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'hello');
+        assert.equal(getLines(results[0].result.stdout).slice(-2)[0], 'hello');
         assert.ok(!results[1].error);
         assert.ok(isError(results[1].result));
-        assert.ok(isVersion(cr(results[2].result.stdout).split('\n').slice(-2, -1)[0].split(':')[1], 'v'));
+        assert.ok(isVersion(getLines(results[2].result.stdout).slice(-2)[0], 'v'));
         done();
       });
     });
