kmalakoff/keyv-registry

Migrated to void and simplified compat layer

src/index.ts
@@ -41,18 +41,11 @@ export default function createStore<T>(uri: string, callback: CreateStoreCallbac
 export default function createStore<T>(uri: string, options: CreateStoreOptions, callback: CreateStoreCallback<T>): void;
 export default function createStore<T>(uri: string, options?: CreateStoreOptions): Promise<Keyv<T>>;
 export default function createStore<T>(uri: string, options?: CreateStoreOptions | CreateStoreCallback<T>, callback?: CreateStoreCallback<T>): void | Promise<Keyv<T>> {
-  // Normalize arguments
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-  options = options || {};
+  callback = typeof options === 'function' ? options : callback;
+  options = typeof options === 'function' ? {} : ((options || {}) as CreateStoreOptions);
 
-  // Callback mode
   if (typeof callback === 'function') return worker(uri, options, callback);
-
-  // Promise mode
   return new Promise((resolve, reject) => {
-    worker(uri, options as CreateStoreOptions, (err, store) => (err ? reject(err) : resolve(store as Keyv<T>)));
+    worker(uri, options, (err, store) => (err ? reject(err) : resolve(store as Keyv<T>)));
   });
 }

kmalakoff/dis-dat

src/index.ts
@@ -10,15 +10,12 @@ export default function disDat(commands: string[], callback: DisDatCallback): vo
 export default function disDat(commands: string[], options: DisDatOptions, callback: DisDatCallback): void;
 
 export default function disDat(commands: string[], options?: DisDatOptions | DisDatCallback, callback?: DisDatCallback): void | Promise<DisDatResult[]> {
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-  options = options || {};
+  callback = typeof options === 'function' ? options : callback;
+  options = typeof options === 'function' ? {} : ((options || {}) as DisDatOptions);
 
   if (typeof callback === 'function') return worker(commands, options, callback);
   return new Promise((resolve, reject) =>
-    worker(commands, options as DisDatOptions, (err?: DisDatError, results?: DisDatResult[]): void => {
+    worker(commands, options, (err?: DisDatError, results?: DisDatResult[]): void => {
       err ? reject(err) : resolve(results);
     })
   );